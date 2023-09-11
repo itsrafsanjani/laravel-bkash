92f58d91b8346302bca8f72456640c9d6ae00d75
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2023-09-11 09:31:05 +0600

    wip: createPayment in progress

--- config/bkash.php
+++ config/bkash.php
@@ -2,5 +2,13 @@
 
 // config for ItsRafsanJani/Bkash
 return [
+    'sandbox' => env('BKASH_SANDBOX', true),
 
+    'app_key' => env('BKASH_APP_KEY', ''),
+    'app_secret' => env('BKASH_APP_SECRET', ''),
+    'username' => env('BKASH_USERNAME', ''),
+    'password' => env('BKASH_PASSWORD', ''),
+
+    'callbackURL' => env('BKASH_CALLBACK_URL', 'http://127.0.0.1:8000/bkash/callback'),
+    'timezone' => 'Asia/Dhaka',
 ];
--- src/Bkash.php
+++ src/Bkash.php
@@ -2,6 +2,120 @@
 
 namespace ItsRafsanJani\Bkash;
 
+use Illuminate\Support\Facades\Http;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
 class Bkash
 {
+    /**
+     * @var string $baseUrl
+     */
+    public string $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl();
+    }
+
+    /**
+     * bkash Base Url
+     * if sandbox is true it will be sandbox url otherwise it is host url
+     */
+    public function baseUrl()
+    {
+        if (config('bkash.sandbox')) {
+            $this->baseUrl = 'https://tokenized.sandbox.bka.sh/v1.2.0-beta/tokenized';
+        } else {
+            $this->baseUrl = 'https://tokenized.pay.bka.sh/v1.2.0-beta/tokenized';
+        }
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIp()
+    {
+        return request()->ip();
+    }
+
+
+    /**
+     * @return array|mixed
+     * @throws \Exception
+     */
+    public function getToken()
+    {
+        session()->forget('bkash_token');
+        session()->forget('bkash_token_type');
+        session()->forget('bkash_refresh_token');
+
+        $appKey = config('bkash.app_key');
+        $appSecret = config('bkash.app_secret');
+        $refreshToken = null; // Set your refresh token here
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'password' => config('bkash.password'),
+            'username' => config('bkash.username'),
+        ])->post("$this->baseUrl/checkout/token/grant", [
+            'app_key' => $appKey,
+            'app_secret' => $appSecret,
+            'refresh_token' => $refreshToken,
+        ])->json();
+
+
+        $this->throwIfError($response);
+
+        session()->put('bkash_token', $response['id_token']);
+        session()->put('bkash_token_type', $response['token_type']);
+        session()->put('bkash_refresh_token', $response['refresh_token']);
+
+        return true;
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    protected function headers()
+    {
+        return [
+            'Content-Type' => 'application/json',
+            'Authorization' => session()->get('bkash_token'),
+            'X-APP-KEY' => config('bkash.app_key'),
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @return array|mixed
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws \Exception
+     */
+    public function createPayment(array $data)
+    {
+        $this->getToken();
+
+        $response = Http::withHeaders($this->headers())
+            ->post("$this->baseUrl/checkout/create", $data)
+            ->json();
+
+        $this->throwIfError($response);
+
+        return $response;
+    }
+
+    /**
+     * @param array $response
+     * @return void
+     * @throws \Exception
+     */
+    public function throwIfError(array $response): void
+    {
+        if ($response['statusCode'] !== 0000) {
+            throw new \Exception($response['statusMessage']);
+        }
+    }
 }
