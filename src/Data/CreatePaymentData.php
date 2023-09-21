37e031498333f432049c187fb8a479da9944dff6
itsrafsanjani <itsrafsanjani@users.noreply.github.com>
2023-09-21 15:57:07 +0000

    Fix styling

--- src/Data/CreatePaymentData.php
+++ src/Data/CreatePaymentData.php
@@ -7,16 +7,16 @@ use Spatie\LaravelData\Data;
 class CreatePaymentData extends Data
 {
     public string $callbackURL;
+
     public string $merchantInvoiceNumber;
 
     public function __construct(
-        public float  $amount,
+        public float $amount,
         public string $payerReference,
-        public string $mode  = '0011', // This parameter indicates the mode of payment. For Tokenized Checkout, the value of this parameter should be "0001".,
+        public string $mode = '0011', // This parameter indicates the mode of payment. For Tokenized Checkout, the value of this parameter should be "0001".,
         public string $currency = 'BDT',
         public string $intent = 'sale',
-    )
-    {
+    ) {
         $this->callbackURL = config('bkash.callbackURL');
         $this->merchantInvoiceNumber = $this->payerReference;
     }
